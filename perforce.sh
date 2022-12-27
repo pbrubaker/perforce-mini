583970472393dc58fb2f2006c8efab2f2103fdf1
Pete Brubaker <pbrubaker@gmail.com>
2022-12-26 20:08:26 -0800

    Allow existing group ID, add env vars for Unicode and case Insensitivity
    Allow existing group ID, by adding -o to groupmod.
    
    Add CASE_INSENSITIVE and UNICODE environment variables for Unicode and case Insensitivity settings.  These will accept 'true/false or 1/0

--- perforce.sh
+++ perforce.sh
@@ -4,7 +4,7 @@ set -e
 # change user-id of perforce user to match the host user-id
 usermod -u $PERFORCE_UID perforce
 # change group-id of perforce group to match the host group-id
-groupmod -g $PERFORCE_GID perforce
+groupmod -g -o $PERFORCE_GID perforce
 
 # if the perforce config file doesn't exist, run the config script
 if [ ! -f /etc/perforce/p4dctl.conf.d/perforce.conf ]; then
@@ -15,7 +15,17 @@ if [ ! -f /etc/perforce/p4dctl.conf.d/perforce.conf ]; then
     echo "Generating random master password..."
     MASTER_PASSWORD=$(pwgen -s 32 1)
     echo "Master password: $MASTER_PASSWORD"
-    /opt/perforce/sbin/configure-helix-p4d.sh $SERVER_ID -n -p ssl:$P4PORT -r $P4ROOT -u $MASTER_USER -P $MASTER_PASSWORD --unicode 
+
+    ADDITIONAL_COMMANDS = ""
+    if [ "$CASE_INSENSITIVE" == "1" ] || [ "$CASE_INSENSITIVE" == "true" ] then
+        ADDITIONAL_COMMANDS = "-C1"
+    fi
+
+    if [ "$UNICODE" == "1" ] || [ "$UNICODE" == "true" ] then
+        ADDITIONAL_COMMANDS += " --unicode"
+    fi
+
+    /opt/perforce/sbin/configure-helix-p4d.sh $SERVER_ID -n -p ssl:$P4PORT -r $P4ROOT -u $MASTER_USER -P $MASTER_PASSWORD $ADDITIONAL_COMMANDS
 fi
 
 # if there are no SSL certificates, generate them
